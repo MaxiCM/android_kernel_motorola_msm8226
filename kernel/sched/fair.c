a92ec7e8c146db194be003a582e57c154ee6a863
Krizthian <cristianortizmorales2013@gmail.com>
2015-12-20 10:53:54 -0500

    Revert "vm: lowered latency to reduce lags"
    This reverts commit b241443464f2cfe3afa0b3b40296413aba8b1f63.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -33,11 +33,7 @@
 
 /*
  * Targeted preemption latency for CPU-bound tasks:
-<<<<<<< HEAD
  * (default: 6ms * (1 + ilog(ncpus)), units: nanoseconds)
-=======
- * (default: 5ms * (1 + ilog(ncpus)), units: nanoseconds)
->>>>>>> 8ff4136... vm: lowered latency to reduce lags
  *
  * NOTE: this latency value is not the same as the concept of
  * 'timeslice length' - timeslices in CFS are of variable length
@@ -47,13 +43,8 @@
  * (to see the precise effective timeslice length of your workload,
  *  run vmstat and monitor the context-switches (cs) field)
  */
-<<<<<<< HEAD
 unsigned int sysctl_sched_latency = 6000000ULL;
 unsigned int normalized_sysctl_sched_latency = 6000000ULL;
-=======
-unsigned int sysctl_sched_latency = 5000000ULL;
-unsigned int normalized_sysctl_sched_latency = 5000000ULL;
->>>>>>> 8ff4136... vm: lowered latency to reduce lags
 
 /*
  * The initial- and re-scaling of tunables is configurable
